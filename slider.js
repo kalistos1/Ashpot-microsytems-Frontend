2d595367cc4f04c7738d07f85e5e93cf1d2e2a98
kalistos1 <ucktech1@gmail.com>
2023-07-21 16:27:17 +0100

    initial commit

---
+++ slider.js
@@ -0,0 +1,12 @@
+    $(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,3],
+        itemsDesktopSmall:[980,2],
+        itemsTablet:[768,2],
+        pagination:true,
+        navigation:true,
+        navigationText:["<",">"],
+        autoPlay:true
+    });
+});
